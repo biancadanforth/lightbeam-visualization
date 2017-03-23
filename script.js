d318aa0dff4f3116ee084df10d66d9fb9dc3fb1c
Bianca Danforth <biancadanforth@gmail.com>
2017-03-22 19:52:43 -0700

    Refactored script.js

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
-// This script imports and interprets lightbeamData.json
+/* This script imports and utilizes lightbeamData.json to make a prototype Lightbeam visualization application | see: https://github.com/mozilla/lightbeam/issues/710 for more information.*/
 
-/* LOAD DATA */
-// resource: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+function lightbeamVis() {
+	
+	let visitedSites = {
+		'google.com': {},
+		'amazon.com': {},
+		'facebook.com': {},
+		'youtube.com': {},
+	};
 
-// The lightbeamData.json file is an array of Connection objects (also arrays) output by Lightbeam's 'Save Data' option. Each array element is of the form:
-//   var theLog = [
-//     Connection.source (string),
-//     Connection.target (string),
-//     Connection.timestamp.valueOf() (number),
-//     Connection.contentType (string),
-//     Connection.cookie (boolean),
-//     Connection.sourceVisited (boolean),
-//     Connection.secure (boolean),
-//     Connection.sourcePathDepth (number),
-//     Connection.sourceQueryDepth (number),
-//     Connection.sourceSubdomain (string),
-//     Connection.targetSubdomain (string),
-//     Connection.method (string),
-//     Connection.statusCode (number),
-//     Connection.cacheable (boolean),
-//     Connection._sourceTab.isPrivate (boolean)
-//   ];
-// see: https://github.com/mozilla/lightbeam/issues/710 for more info.
+	let thirdPartySpan = document.getElementById("third-party-site");
+	let thirdPartyAboutSpan = document.getElementById("third-party-site-about");
+	let visitedSiteSpan = document.getElementById("visited-site");
+	let prevPathElement;
+	let sidePanel = document.getElementById('side-panel');
 
-// Constants for indexes of properties in array format
-// Connection.SOURCE = 0;
-// Connection.TARGET = 1;
-// Connection.TIMESTAMP = 2;
-// Connection.CONTENT_TYPE = 3;
-// Connection.COOKIE = 4;
-// Connection.SOURCE_VISITED = 5;
-// Connection.SECURE = 6;
-// Connection.SOURCE_PATH_DEPTH = 7;
-// Connection.SOURCE_QUERY_DEPTH = 8;
-// Connection.SOURCE_SUB = 9;
-// Connection.TARGET_SUB = 10;
-// Connection.METHOD = 11;
-// Connection.STATUS = 12;
-// Connection.CACHEABLE = 13;
-// Connection.FROM_PRIVATE_MODE = 14;
+		// get <path> element from DOM based on title attribute
+	let pathElements = [];
+	let pathSVGElements = document.getElementsByClassName('site');
+	for (let k = 0; k < pathSVGElements.length; k++) {
+		pathElements.push(pathSVGElements[k].getElementsByTagName('path'));
+	}
+	//convert array of HTML collection objects into a single array of path elements
+	pathElements = pathElements.reduce(function(a, b) {return [...a, ...b];
+		}, []);
 
-let siteData = init();
-console.log(siteData);
+	// add click event to each third party pie section (pathElement)
+	for (let l = 0; l < pathElements.length; l++) {
+		pathElements[l].addEventListener('click', function() {
+				updateSideBar(pathElements[l]);
+			}
+		);
+	}
 
-function init() {
-	loadJSON(function(response) {
+	/* -------- LOAD DATA ---------- */
+
+	loadJSON();
+
+	function loadJSON() {   
+		var xobj = new XMLHttpRequest();
+	  xobj.overrideMimeType("application/json");
+	  xobj.open('GET', 'lightbeamData.json', true);
+	  xobj.onreadystatechange = function () {
+	        if (xobj.readyState == 4 && xobj.status == "200") {
+	          init(xobj.responseText);
+	        }
+	  };
+	  xobj.send(null);  
+	}
+
+	function init(response) {
+		
 		// Parse JSON string into object
 		var data = JSON.parse(response);
 		
-		let visitedSites = {
-			'google.com': {},
-			'amazon.com': {},
-			'facebook.com': {},
-			'youtube.com': {},
-		};
-
 		// populate visitedSites from JSON data
 		// each element in each array is a unique third-party site linked to by the visited site key.
 		for (let currentSite in visitedSites) {
@@ -67,71 +66,35 @@ function init() {
 				if (source === currentSite && target !== currentSite) {
 					// if third-party site is not yet captured
 					if (!visitedSites[currentSite].hasOwnProperty(target)) {
-						visitedSites[currentSite][target] = {};
+						visitedSites[currentSite][target] = [];
 					}
 				}
 			}
 		}
 
 		console.log('Third party sites by visited site: ', visitedSites);
+	}
 
-		// get path element from DOM based on title attribute
-		let pathElements = [];
-
-		let pathSVGElements = document.getElementsByClassName('site');
-		console.log(pathSVGElements);
-
-		for (let k = 0; k < pathSVGElements.length; k++) {
-			pathElements.push(pathSVGElements[k].getElementsByTagName('path'));
+	function updateSideBar(pathElement) {
+		if (sidePanel.classList.contains('hidden')) {
+			sidePanel.classList.remove('hidden');
 		}
-
-		//convert array of HTML collection objects into a single array of path elements
-		pathElements = pathElements.reduce(function(a, b) {return [...a, ...b];
-}, []);
-
-		for (let l = 0; l < pathElements.length; l++) {
-			pathElements[l].addEventListener('click', function() {
-					updateSideBar(pathElements[l]);
-				}
-			);
+		if (!prevPathElement) {
+			prevPathElement = pathElement;
+		} else if (prevPathElement !== pathElement) {
+			prevPathElement.classList.remove('active');
+			prevPathElement = pathElement;
 		}
-
-		let thirdPartySpan = document.getElementById("third-party-site");
-		let thirdPartyAboutSpan = document.getElementById("third-party-site-about");
-		let visitedSiteSpan = document.getElementById("visited-site");
-		let prevPathElement;
-
-		function updateSideBar(pathElement) {
-			if (!prevPathElement) {
-				prevPathElement = pathElement;
-			} else if (prevPathElement !== pathElement) {
-				prevPathElement.classList.remove('active');
-				prevPathElement = pathElement;
-			}
-				thirdPartySpan.innerText = pathElement.textContent;
-			thirdPartyAboutSpan.innerText = pathElement.textContent;
-			// get parentElement to pathElement (<svg>)
-			let parentSVG = pathElement.nearestViewportElement;
-			// then get siblingElement to parentElement (<a>)
-			let siblingElement = parentSVG.nextElementSibling;
-			// then get the value of the title attribute
-			visitedSiteSpan.innerText = siblingElement.getAttribute('title');
-			pathElement.classList.add('active');
-		}
-	});
+			thirdPartySpan.innerText = pathElement.textContent;
+		thirdPartyAboutSpan.innerText = pathElement.textContent;
+		// get parentElement to pathElement (<svg>)
+		let parentSVG = pathElement.nearestViewportElement;
+		// then get siblingElement to parentElement (<a>)
+		let siblingElement = parentSVG.nextElementSibling;
+		// then get the value of the title attribute
+		visitedSiteSpan.innerText = siblingElement.getAttribute('title');
+		pathElement.classList.add('active');
+	}
 }
 
-function loadJSON(callback) {   
-	var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'lightbeamData.json', true);
-  xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-          callback(xobj.responseText);
-        }
-  };
-  xobj.send(null);  
-}
-
-
+window.onload = lightbeamVis;
